52cb5372cbb67127f7eb5a17c9f1bdc2c0c44b8b
Heiner Samuel <xxxheinersamuelxxx@gmail.com>
2025-09-18 20:58:16 -0500

    ultimo commit

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+    function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = "";
+    actualizarLista();
+ } function actualizarLista(){ 
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (const amigo of amigos) {
+        const li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    }  }
+    function sortearAmigo() {
+        if (amigos.length == 0) {
+            alert("No hay amigos para sortear.");
+            return;
+        }
+        const amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+        const resultado = document.getElementById("resultado");
+        resultado.textContent = "El amigo secreto es: " + amigoSorteado;
+         }
+ ; 
